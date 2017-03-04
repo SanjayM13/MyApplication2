8f66630e871c27be5b4b120aaebadb6311ccf272
Sanjay <imsanjay13@gmail.com>
2017-03-04 18:56:47 +0530

    Added buttons and made sure when orientation is changed, components are well aligned

--- app/src/main/java/com/example/sanjay/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sanjay/myapplication/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.sanjay.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    static int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+        Button Inc = (Button) findViewById(R.id.inc_button);
+        Inc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                firstTextView.setText(" " + count);
+            }
+        });
+
+        Button Dec = (Button) findViewById(R.id.dec_button);
+        Dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count--;
+                firstTextView.setText(" " + count);
+            }
+        });
     }
 }
